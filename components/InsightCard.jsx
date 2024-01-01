a449fb2ec2d5811b93a0c147726cfbc5efee04af
EHAB3x <ehabmah6969@gmail.com>
2024-01-01 20:57:48 +0200

    finish insight section and insight card component

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,32 @@
 'use client';
 
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
+
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4 items-center"
+  >
+    <img
+      src={imgUrl}
+      alt="planet-01"
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+    />
+    <div className="w-full flex justify-between items-center">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
+        <h4 className="font-normal lg:text-[30px] text-[26px] text-white">{title}</h4>
+        <p className="mt-[16px] font-normal lg:text-[18px] text-[14px] text-secondary-white">{subtitle}</p>
+      </div>
+    </div>
+    <div className="lg:flex hidden items-center justify-center min-w-[70px] w-[70px] h-[70px] rounded-full bg-transparent border-[1px] border-white hover:bg-secondary-white transition-flex duration-[0.3s]">
+      <img
+        src="arrow.svg"
+        alt="arrow"
+        className="w-[40%] h-[40%] object-contain"
+      />
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
